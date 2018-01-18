ed0bcc7c87f08e218eecdf6d9321b1bbab314a0d
BoyWithSilverWings <agney.menons@live.com>
2018-01-19 00:18:55 +0530

    Setting up a sample graph

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons'
+import Chart from 'chart.js';
+import './index.scss';
 
 UIkit.use(Icons);
+
+const ctx = document.getElementById("chart");
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Red", "Blue"],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
 
 console.log("Done");
