33471b6d710a0cd98893b5f37545f37c1acddb59
BoyWithSilverWings <agney.menons@live.com>
2018-01-21 14:58:32 +0530

    API calling function

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const SEARCH_GITHUB_URL = 'https://api.github.com/search/repositories';
+
+
+function getInfo(package) {
+  return fetch(SEARCH_GITHUB_URL, {
+    query: {
+      q: package,
+      page: 1,
+      per_page: 1
+    }
+  })
+  .then(res=>response.json())
+  .then(res=>res)
+  .catch(err=>err);
+}
